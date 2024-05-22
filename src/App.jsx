82417139b51decceda6e544fd64e873dc0f13018
Anagha Chadayan <anagha.chadayan@gmail.com>
2024-05-22 16:36:07 +0200

    router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import MainLayout from "./layout/MainLayout";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,10 @@
+import ThemeToggler from "./ThemeToggler";
+const NavBar = () => {
+  return (
+    <div>
+      <ThemeToggler />
+    </div>
+  );
+};
+
+export default NavBar;
