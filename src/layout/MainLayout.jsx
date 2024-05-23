8e02ea1ad51798fea046fa630db408ca76b839ae
javiMolcon <javier.molina.conesa@gmail.com>
2024-05-23 10:56:04 +0200

    first HeroSection commit

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
+import HeroSection from "../components/HeroSection";
 
 const MainLayout = () => {
   return (
     <div>
       <NavBar />
+      <HeroSection />
       <Outlet />
       <Footer />
     </div>
