caf223796d2d372afce94744c6067d5009cea8c1
DipaliTalware <d.talware@gmail.com>
2024-05-23 14:57:49 +0200

    contact form is created

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,9 @@ const Footer = () => {
 		<footer className='footer footer-center p-10 bg-base-200 text-base-content rounded'>
 			<nav className='grid grid-flow-col gap-4'>
 				<Link className='link link-hover'>About us</Link>
-				<Link className='link link-hover'>Contact</Link>
+				<Link to='/contact' className='link link-hover'>
+					Contact
+				</Link>
 				<Link to='/faq' className='link link-hover'>
 					FAQ
 				</Link>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,74 @@
+import { useState } from 'react';
+
 const Contact = () => {
-	return <div>Contact</div>;
+	const [formData, setFormData] = useState({
+		name: '',
+		email: '',
+		phone: '',
+		message: '',
+	});
+
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setFormData({ ...formData, [name]: value });
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setFormData({
+			name: '',
+			email: '',
+			phone: '',
+			message: '',
+		});
+	};
+
+	return (
+		<div className='max-w-lg mx-auto p-8 bg-base-200 shadow-md rounded-lg mt-28 mb-8'>
+			<h2 className='text-2xl font-bold mb-6 text-center'>Contact Us</h2>
+			<form onSubmit={handleSubmit}>
+				<div className='mb-4'>
+					<input
+						type='text'
+						name='name'
+						value={formData.name}
+						onChange={handleChange}
+						placeholder='Your Name'
+						className='w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500'
+						required
+					/>
+				</div>
+				<div className='mb-4'>
+					<input
+						type='email'
+						name='email'
+						value={formData.email}
+						onChange={handleChange}
+						placeholder='Your Email'
+						className='w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500'
+						required
+					/>
+				</div>
+				<div className='mb-6'>
+					<textarea
+						name='message'
+						value={formData.message}
+						onChange={handleChange}
+						placeholder='Your Message'
+						rows='5'
+						className='w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500'
+						required
+					></textarea>
+				</div>
+				<button
+					type='submit'
+					className='w-full px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600'
+				>
+					Submit
+				</button>
+			</form>
+		</div>
+	);
 };
 
 export default Contact;
