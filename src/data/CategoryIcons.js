8b00f91c6050c3b7dc2f6758c7ea77eba02f52f5
Anagha Chadayan <anagha.chadayan@gmail.com>
2024-06-04 11:15:05 +0200

    removed unwanted icons

--- src/data/CategoryIcons.js
+++ src/data/CategoryIcons.js
@@ -2,12 +2,7 @@ import {
   FaMusic,
   FaGamepad,
   FaGraduationCap,
-  FaBook,
   FaBuilding,
-  FaShieldAlt,
-  FaBriefcaseMedical,
-  FaCloud,
-  FaLaptopCode,
 } from "react-icons/fa";
 import { MdOutlineSportsSoccer, MdLocalMovies, MdApps } from "react-icons/md";
 import { IoDocumentText } from "react-icons/io5";
@@ -19,14 +14,8 @@ const categoryIcons = {
   Entertainment: MdLocalMovies,
   Gaming: FaGamepad,
   "e-Learning": FaGraduationCap,
-  Reading: FaBook,
   Other: IoDocumentText,
-  other: IoDocumentText,
   Office: FaBuilding,
-  Security: FaShieldAlt,
-  Software: FaLaptopCode,
-  Cloud: FaCloud,
-  Health: FaBriefcaseMedical,
 };
 
 export default categoryIcons;
