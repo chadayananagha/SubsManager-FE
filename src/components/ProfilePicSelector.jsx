f52ae134db85f83b1e7264dddc2be4d6a2f014f2
javiMolcon <javier.molina.conesa@gmail.com>
2024-06-06 15:30:04 +0200

    added profile card with profile editing and profilePic upload

---
+++ src/components/ProfilePicSelector.jsx
@@ -0,0 +1,164 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Image1 from "../../public/images/User1.png";
+import Image2 from "../../public/images/User2.png";
+import Image3 from "../../public/images/User3.png";
+import Image4 from "../../public/images/User4.png";
+import Image5 from "../../public/images/User5.png";
+import Image6 from "../../public/images/User6.png";
+import Image7 from "../../public/images/User7.png";
+import Image8 from "../../public/images/User8.png";
+import Image9 from "../../public/images/User9.png";
+import Image10 from "../../public/images/User10.png";
+
+const ProfilePicSelector = ({ userData, setUserData, userId }) => {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [imagePreviewUrl, setImagePreviewUrl] = useState(null);
+  const [isUploading, setIsUploading] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState(null);
+
+  const presetImages = [
+    Image2,
+    Image3,
+    Image4,
+    Image5,
+    Image6,
+    Image7,
+    Image8,
+    Image9,
+    Image10,
+  ];
+
+  const localAPI = "http://localhost:8080";
+  const deployedAPI = "https://subsmanager-be.onrender.com";
+
+  const handleImageClick = async (image, index) => {
+    try {
+      const response = await fetch(image);
+      const blob = await response.blob();
+      const file = new File([blob], `preset-${image.split("/").pop()}`, {
+        type: blob.type,
+      });
+      setSelectedImage(file);
+      setImagePreviewUrl(URL.createObjectURL(file)); // Set the preview URL
+      setSelectedIndex(index);
+    } catch (error) {
+      console.error("Error converting image to file:", error);
+    }
+  };
+
+  const handleFileSelect = (e) => {
+    e.preventDefault();
+    const file = e.target.files[0];
+    setSelectedImage(file);
+    setImagePreviewUrl(URL.createObjectURL(file)); // Set the preview URL
+    setSelectedIndex(null);
+  };
+
+  const uploadProfilePic = async () => {
+    if (!selectedImage) return;
+    try {
+      setIsUploading(true);
+
+      const formData = new FormData();
+      formData.append("url", selectedImage);
+
+      const response = await axios.post(
+        `${localAPI}/users/upload/${userId}/`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      setUserData({
+        ...userData,
+        profilePic: imagePreviewUrl,
+      });
+
+      setIsUploading(false);
+      setIsModalOpen(false); // Close the modal after uploading
+    } catch (error) {
+      console.error("Error uploading profile picture:", error);
+      setIsUploading(false);
+    }
+  };
+
+  return (
+    <div>
+      <img
+        src={userData.profilePic.url || imagePreviewUrl}
+        alt="User profile pic"
+        className="w-32 rounded-full shadow-lg cursor-pointer hover:"
+        onClick={() => setIsModalOpen(true)}
+      />
+
+      {/* Modal */}
+      {isModalOpen && (
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50 z-50">
+          <div className="bg-white p-4 rounded-lg shadow-lg max-w-lg relative">
+            <button
+              onClick={() => setIsModalOpen(false)}
+              className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+            <h2 className="text-lg font-semibold mb-4">
+              Select Profile Picture
+            </h2>
+            <div className="grid grid-cols-3 gap-2 mb-4">
+              {presetImages.map((image, index) => (
+                <img
+                  key={index}
+                  src={image}
+                  alt={`Preset Image ${index + 1}`}
+                  className={`w-28 h-28 object-cover rounded-full shadow-lg cursor-pointer ${
+                    selectedIndex === index ? "border-4 border-blue-500" : ""
+                  }`}
+                  onClick={() => handleImageClick(image, index)}
+                />
+              ))}
+            </div>
+            <input
+              type="file"
+              accept="image/*"
+              onChange={handleFileSelect}
+              className="hidden"
+              id="file-input"
+            />
+            <label htmlFor="file-input">
+              <div className="w-full shadow-lg cursor-pointer flex items-center justify-center  bg-base-200 hover:bg-base-300 p-2 rounded">
+                Browse
+              </div>
+            </label>
+            <button
+              onClick={uploadProfilePic}
+              disabled={!selectedImage || isUploading}
+              className="btn btn-primary text-white font-bold py-2 px-4 rounded mt-8"
+            >
+              {isUploading ? "Uploading..." : "Upload"}
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ProfilePicSelector;
--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,15 +1,202 @@
+import React, { useContext, useState, useEffect } from "react";
+import axios from "axios";
 import { useJwt } from "react-jwt";
 import { AuthContext } from "../context/AuthContext";
-import { useContext } from "react";
+import { FaEdit } from "react-icons/fa";
+import ProfilePicSelector from "../components/ProfilePicSelector";
+
 const UserProfile = () => {
   const { userId, token } = useContext(AuthContext);
   const { decodedToken } = useJwt(token);
   const username = decodedToken?.username;
-  // console.log(decodedToken);
+
+  const [userData, setUserData] = useState({
+    firstName: "",
+    lastName: "",
+    country: "",
+    email: "",
+    profilePic: "",
+    profileCompletionScore: 0,
+  });
+
+  const [isEditing, setIsEditing] = useState(false);
+
+  const localAPI = "http://localhost:8080";
+  const deployedAPI = "https://subsmanager-be.onrender.com";
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await axios.get(`${localAPI}/users/${userId}`);
+        console.log("response", response);
+        setUserData(response.data);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+
+    if (userId) {
+      fetchUserData();
+    }
+  }, [userId]);
+
+  const handleEditChange = (e) => {
+    setUserData({
+      ...userData,
+      [e.target.name]: e.target.value,
+    });
+  };
+  console.log("wholeUser", userData);
+
+  const handleSave = async () => {
+    try {
+      const response = await axios.put(`${localAPI}/users/${userId}`, userData);
+      console.log("response after editing", response.data);
+      setUserData(response.data);
+      setIsEditing(false);
+    } catch (error) {
+      console.error("Error updating user data:", error);
+    }
+  };
+
   return (
     <div>
-      <div className="bg-green-300">{username}</div>
-      <div className="bg-blue-300">{userId}</div>
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="overflow-hidden shadow rounded-lg bg-base-200 w-[600px]">
+          <div className="px-4 py-5 sm:px-6 flex gap-4">
+            <ProfilePicSelector
+              userId={userId}
+              userData={userData}
+              setUserData={setUserData}
+            />
+            <h3 className="text-3xl font-semibold text-primary">{username}</h3>
+            <button
+              onClick={() => setIsEditing(true)}
+              className="ml-auto btn btn-primary rounded-md shadow-sm text-black"
+            >
+              Edit Profile info
+              <span>
+                <FaEdit />
+              </span>
+            </button>
+          </div>
+          <div className="border-t border-primary px-4 py-5 sm:p-0">
+            <dl className="sm:divide-y sm:divide-gray-200">
+              <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">
+                  First name
+                </dt>
+                <dd className="mt-1 text-sm sm:mt-0 sm:col-span-2">
+                  {userData.firstName}
+                </dd>
+              </div>
+              <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">Last name</dt>
+                <dd className="mt-1 text-sm sm:mt-0 sm:col-span-2">
+                  {userData.lastName}
+                </dd>
+              </div>
+              <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">
+                  Email address
+                </dt>
+                <dd className="mt-1 text-sm sm:mt-0 sm:col-span-2">
+                  {userData.email}
+                </dd>
+              </div>
+              <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">Country</dt>
+                <dd className="mt-1 text-sm sm:mt-0 sm:col-span-2">
+                  {userData.country}
+                </dd>
+              </div>
+            </dl>
+          </div>
+        </div>
+      </div>
+
+      {isEditing && (
+        <div className="fixed inset-0 flex items-center justify-center z-50 ">
+          <div className="bg-base-100 rounded-lg p-6 w-96 shadow-[0_0_0_10000px_rgba(0,0,0,.40)] relative">
+            <button
+              onClick={() => setIsEditing(false)}
+              className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+            <h2 className="text-xl font-semibold mb-4">Edit Profile</h2>
+            <div className="mb-4">
+              <label className="block text-sm font-medium text-gray-700">
+                First name
+              </label>
+              <input
+                type="text"
+                name="firstName"
+                value={userData.firstName}
+                onChange={handleEditChange}
+                className="mt-1 block w-full border border-gray-300 rounded p-2"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-sm font-medium text-gray-700">
+                Last name
+              </label>
+              <input
+                type="text"
+                name="lastName"
+                value={userData.lastName}
+                onChange={handleEditChange}
+                className="mt-1 block w-full border border-gray-300 rounded p-2"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-sm font-medium text-gray-700">
+                Email address
+              </label>
+              <input
+                type="email"
+                name="email"
+                value={userData.email}
+                onChange={handleEditChange}
+                className="mt-1 block w-full border border-gray-300 rounded p-2"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-sm font-medium text-gray-700">
+                Country
+              </label>
+              <input
+                type="text"
+                name="country"
+                value={userData.country}
+                onChange={handleEditChange}
+                className="mt-1 block w-full border border-gray-300 rounded p-2"
+              />
+            </div>
+            <div className="flex justify-end gap-2">
+              <button
+                onClick={handleSave}
+                className="btn btn-primary text-white font-bold py-2 px-4 rounded"
+              >
+                Save
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
