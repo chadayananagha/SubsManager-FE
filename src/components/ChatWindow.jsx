10ffbb7d3420faeac1e9ade303e7cb21b85ad7f2
DipaliTalware <d.talware@gmail.com>
2024-06-07 11:01:38 +0200

    chat window created

--- src/components/ChatWindow.jsx
+++ src/components/ChatWindow.jsx
@@ -1,105 +1,155 @@
-import React, { useState, useEffect, useContext } from "react";
-import { AuthContext } from "../context/AuthContext";
-import { io } from "socket.io-client";
+import React, { useState, useEffect, useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
+import { io } from 'socket.io-client';
+import axios from 'axios';
 
 const ChatWindow = ({ onClose, receiver }) => {
-  const [message, setMessage] = useState("");
-  const [chatContent, setChatContent] = useState([]);
-  const { userId, token } = useContext(AuthContext);
-  const [socket, setSocket] = useState(null);
+	const [message, setMessage] = useState('');
+	const [chatContent, setChatContent] = useState([]);
+	const { userId, token } = useContext(AuthContext);
+	const [socket, setSocket] = useState(null);
+	const [conversations, setConversations] = useState([]);
+	const [messages, setMessages] = useState([]);
 
-  useEffect(() => {
-    const newSocket = io("http://localhost:8080", {
-      query: { token },
-    });
-    setSocket(newSocket);
+	useEffect(() => {
+		const newSocket = io('http://localhost:8080', {
+			query: { token },
+		});
+		setSocket(newSocket);
 
-    // Handle receiving messages
-    newSocket.on("receiveMessage", (msg) => {
-      if (msg.sender === receiver._id || msg.receiver === userId) {
-        setChatContent((prevChatContent) => [...prevChatContent, msg]);
-      }
-    });
+		// Handle receiving messages
+		newSocket.on('receiveMessage', (msg) => {
+			if (msg.sender === receiver._id || msg.receiver === userId) {
+				setChatContent((prevChatContent) => [...prevChatContent, msg]);
+			}
+		});
 
-    // Cleanup on unmount
-    return () => {
-      newSocket.disconnect();
-    };
-  }, [token, receiver._id, userId]);
+		// Cleanup on unmount
+		return () => {
+			newSocket.disconnect();
+		};
+	}, [token, receiver._id, userId]);
 
-  const handleInputChange = (e) => {
-    setMessage(e.target.value);
-  };
+	const handleInputChange = (e) => {
+		setMessage(e.target.value);
+	};
 
-  const handleSendMessage = () => {
-    if (message.trim() === "") return;
-    if (receiver._id === userId) return;
+	// new  code /// to get conversation history
 
-    const newMessage = {
-      sender: userId,
-      receiver: receiver._id,
-      message: message.trim(),
-      timestamp: new Date(),
-    };
+	useEffect(() => {
+		const getConversationBetweenTwo = async () => {
+			console.log(receiver.username);
+			console.log(userId, receiver._id);
+			try {
+				const response = await axios.get(
+					`http://localhost:8080/conversation/${userId}/${receiver._id}`
+				);
+				console.log(receiver.username);
+				if (response.data) {
+					console.log(response.data);
+					setConversations(response.data);
+					getAllMessages(response.data[0]._id);
+					console.log(response.data);
+				} else {
+					console.log('i am in no chat');
+					setMessages([]);
+				}
+			} catch (error) {
+				console.error(error);
+			}
+		};
+		if (userId) {
+			getConversationBetweenTwo();
+		}
+		console.log(receiver.username);
+	}, [userId, receiver._id]);
 
-    // Emit the message to the server
-    socket.emit("sendMessage", newMessage);
+	// New Code ///////// get chat history
 
-    // Optionally, add the message to the local state immediately
-    setChatContent((prevChatContent) => [...prevChatContent, newMessage]);
-    console.log(newMessage);
+	const getAllMessages = async (conversationsId) => {
+		try {
+			const response = await axios.get(
+				`http://localhost:8080/chat/${conversationsId}`
+			);
 
-    setMessage("");
-  };
+			setMessages(response.data);
+			console.log(response.data);
+		} catch (error) {
+			console.error(error.message);
+		}
+	};
 
-  return (
-    <div className="fixed inset-0 flex justify-center items-center z-50 bg-black bg-opacity-50 px-2">
-      <div className="relative bg-white p-6 rounded-lg shadow-lg max-w-lg w-full">
-        <button
-          className="absolute top-0 right-0 mt-4 mr-4 text-black font-bold text-2xl"
-          onClick={onClose}
-          aria-label="Close"
-        >
-          &times;
-        </button>
-        <div className="font-bold text-xl mb-4">
-          Chat with {receiver.username}
-        </div>
-        <div className="chat-content h-64 overflow-y-auto">
-          {chatContent.map((msg, index) => (
-            <div
-              key={index}
-              className={`message ${
-                msg.sender === userId ? "sent" : "received"
-              }`}
-            >
-              <div className="message-content text-black">
-                <p>{msg.message}</p>
-                <span className="timestamp">
-                  {new Date(msg.timestamp).toLocaleTimeString()}
-                </span>
-              </div>
-            </div>
-          ))}
-        </div>
-        <div className="flex mt-4 gap-2">
-          <input
-            type="text"
-            className="flex-grow border rounded-lg p-2 bg-transparent text-black border-black"
-            placeholder="Type your message..."
-            value={message}
-            onChange={handleInputChange}
-          />
-          <button
-            className="btn bg-primary font-bold py-2 px-4 border-none"
-            onClick={handleSendMessage}
-          >
-            Send
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+	const handleSendMessage = () => {
+		if (message.trim() === '') return;
+		if (receiver._id === userId) return;
+
+		const newMessage = {
+			sender: userId,
+			receiver: receiver._id,
+			message: message.trim(),
+			timestamp: new Date(),
+		};
+
+		// Emit the message to the server
+		socket.emit('sendMessage', newMessage);
+
+		// Optionally, add the message to the local state immediately
+		setChatContent((prevChatContent) => [...prevChatContent, newMessage]);
+		console.log(newMessage);
+
+		setMessage('');
+	};
+
+	return (
+		<div className='fixed inset-0 flex justify-center items-center z-50 bg-black bg-opacity-50 px-2'>
+			<div className='relative bg-white p-6 rounded-lg shadow-lg max-w-lg w-full'>
+				<button
+					className='absolute top-0 right-0 mt-4 mr-4 text-black font-bold text-2xl'
+					onClick={onClose}
+					aria-label='Close'
+				>
+					&times;
+				</button>
+				<div className='font-bold text-xl mb-4'>
+					Chat with {receiver.username}
+				</div>
+				<div className='chat-content h-64 overflow-y-auto'>
+					{messages &&
+						messages.length > 0 &&
+						messages.map((msg, index) => (
+							<div
+								key={index}
+								className={`message ${
+									msg.sender === userId ? 'sent' : 'received'
+								}`}
+							>
+								<div className='message-content text-black'>
+									<p>{msg.message}</p>
+									<span className='timestamp'>
+										{new Date(msg.timestamp).toLocaleTimeString()}
+									</span>
+								</div>
+							</div>
+						))}
+				</div>
+				<div className='flex mt-4 gap-2'>
+					<input
+						type='text'
+						className='flex-grow border rounded-lg p-2 bg-transparent text-black border-black'
+						placeholder='Type your message...'
+						value={message}
+						onChange={handleInputChange}
+					/>
+					<button
+						className='btn bg-primary font-bold py-2 px-4 border-none'
+						onClick={handleSendMessage}
+					>
+						Send
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default ChatWindow;
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,78 +1,81 @@
-import { Link } from "react-router-dom";
-import { useContext } from "react";
-import { AuthContext } from "../context/AuthContext";
-import { toast } from "react-hot-toast";
+import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
+import { toast } from 'react-hot-toast';
 
 const HeroSection = () => {
-  const { token, logout } = useContext(AuthContext);
+	const { token, logout } = useContext(AuthContext);
 
-  //*logout Function
-  const handleLogout = () => {
-    logout();
-    toast.success("Successfully logged out!");
-  };
-  return (
-    <div>
-      <div className="hero  bg-base">
-        <div className="hero-content flex-col lg:flex-row-reverse bg-base">
-          <img
-            className="max-w-sm rounded-lg mix-blend-multiply lg:ml-8"
-            src="/images/heroPic.png"
-            alt="Hero Illustration"
-          />
-          <div className="flex flex-col items-center lg:items-start gap-24">
-            <div className="flex flex-col gap-4">
-              <h1 className="text-7xl font-bold text-primary text-center lg:text-left">
-                Stream smart,
-              </h1>
-              <h2 className="text-7xl font-bold text-center lg:text-left">
-                Share better.
-              </h2>
-              <p className="text-2xl text-center pt-12 lg:text-left text-balance">
-                Combine and manage your subscriptions, simplify life, enjoy
-                more!
-              </p>
-            </div>
-            <div className="flex gap-6 justify-center">
-              {token ? (
-                <div>
-                  <button onClick={handleLogout} className="btn btn-accent">
-                    Log out
-                  </button>
+	//*logout Function
+	const handleLogout = () => {
+		logout();
+		toast.success('Successfully logged out!');
+	};
+	return (
+		<div>
+			<div className='hero  bg-base'>
+				<div className='hero-content flex-col lg:flex-row-reverse bg-base'>
+					<img
+						className='max-w-sm rounded-lg mix-blend-multiply lg:ml-8'
+						src='/images/heroPic.png'
+						alt='Hero Illustration'
+					/>
+					<div className='flex flex-col items-center lg:items-start gap-24'>
+						<div className='flex flex-col gap-4'>
+							<h1 className='text-7xl font-bold text-primary text-center lg:text-left'>
+								Stream smart,
+							</h1>
+							<h2 className='text-7xl font-bold text-center lg:text-left'>
+								Share better.
+							</h2>
+							<p className='text-2xl text-center pt-12 lg:text-left text-balance'>
+								Combine and manage your subscriptions, simplify life, enjoy
+								more!
+							</p>
+						</div>
+						<div className='flex gap-6 justify-center'>
+							{token ? (
+								<div>
+									<button onClick={handleLogout} className='btn btn-accent'>
+										Log out
+									</button>
+									<Link to='/messenger' className='btn btn-accent'>
+										Messages
+									</Link>
 
-                  <Link
-                    to="/startsharing"
-                    className=" relative text-nowrap btn btn-primary   justify-center items-center overflow-hidden group px-8"
-                  >
-                    <span className="   py-4 flex justify-center items-center opacity-100 group-hover:opacity-0 group-hover:-translate-y-full transition-all duration-1000">
-                      Start sharing
-                    </span>
-                    <span className=" py-4 absolute  opacity-0  group-hover:opacity-100  group-hover:flex group-hover:justify-center group-hover:items-center  translate-y-full  group-hover:translate-y-0  transition-all duration-1000 ">
-                      Right now
-                    </span>
-                  </Link>
-                </div>
-              ) : (
-                <>
-                  <Link to="/login">
-                    <button className="btn btn-color text-black text-2xl px-10  flex justify-center items-center">
-                      Login
-                    </button>
-                  </Link>
+									<Link
+										to='/startsharing'
+										className=' relative text-nowrap btn btn-primary   justify-center items-center overflow-hidden group px-8'
+									>
+										<span className='   py-4 flex justify-center items-center opacity-100 group-hover:opacity-0 group-hover:-translate-y-full transition-all duration-1000'>
+											Start sharing
+										</span>
+										<span className=' py-4 absolute  opacity-0  group-hover:opacity-100  group-hover:flex group-hover:justify-center group-hover:items-center  translate-y-full  group-hover:translate-y-0  transition-all duration-1000 '>
+											Right now
+										</span>
+									</Link>
+								</div>
+							) : (
+								<>
+									<Link to='/login'>
+										<button className='btn btn-color text-black text-2xl px-10  flex justify-center items-center'>
+											Login
+										</button>
+									</Link>
 
-                  <Link to="/signup">
-                    <button className="btn btn-primary text-white text-2xl px-10 ">
-                      Sign up
-                    </button>
-                  </Link>
-                </>
-              )}
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+									<Link to='/signup'>
+										<button className='btn btn-primary text-white text-2xl px-10 '>
+											Sign up
+										</button>
+									</Link>
+								</>
+							)}
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default HeroSection;
