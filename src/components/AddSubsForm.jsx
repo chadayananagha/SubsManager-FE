366a1ac37ed515f435ac3a63e367b81958aa2f92
DipaliTalware <d.talware@gmail.com>
2024-06-11 17:43:51 +0200

    added min value to price and max member

--- src/components/AddSubsForm.jsx
+++ src/components/AddSubsForm.jsx
@@ -1,169 +1,175 @@
-import { useState } from "react";
-import { IoMdCloseCircle } from "react-icons/io";
-import { motion } from "framer-motion";
-import { AiOutlineCloseCircle } from "react-icons/ai";
+import { useState } from 'react';
+import { IoMdCloseCircle } from 'react-icons/io';
+import { motion } from 'framer-motion';
+import { AiOutlineCloseCircle } from 'react-icons/ai';
 
 const AddSubsForm = ({ closeForm, handleSubmit }) => {
-  const [formData, setFormData] = useState({
-    category: "",
-    platformName: "",
-    plan: {
-      planName: "",
-      price: "",
-      maxMembers: "",
-    },
-    expirationDate: "",
-    public: false,
-  });
+	const [formData, setFormData] = useState({
+		category: '',
+		platformName: '',
+		plan: {
+			planName: '',
+			price: '',
+			maxMembers: '',
+		},
+		expirationDate: '',
+		public: false,
+	});
 
-  const handlePublic = (e) => {
-    setFormData((prevData) => ({
-      ...prevData,
-      public: e.target.checked,
-    }));
-  };
+	const handlePublic = (e) => {
+		setFormData((prevData) => ({
+			...prevData,
+			public: e.target.checked,
+		}));
+	};
 
-  const handleChange = (e) => {
-    const { name, value, type } = e.target;
+	const handleChange = (e) => {
+		const { name, value, type } = e.target;
 
-    if (type === "checkbox") {
-      setFormData((prevData) => ({
-        ...prevData,
-        [name]: e.target.checked,
-      }));
-    } else {
-      setFormData((prevData) => ({ ...prevData, [name]: value }));
-    }
-  };
+		if (type === 'checkbox') {
+			setFormData((prevData) => ({
+				...prevData,
+				[name]: e.target.checked,
+			}));
+		} else {
+			setFormData((prevData) => ({ ...prevData, [name]: value }));
+		}
+	};
 
-  const handlePlanChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      plan: { ...prevData.plan, [name]: value },
-    }));
-  };
+	const handlePlanChange = (e) => {
+		const { name, value } = e.target;
+		setFormData((prevData) => ({
+			...prevData,
+			plan: { ...prevData.plan, [name]: value },
+		}));
+	};
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-    handleSubmit(formData);
-  };
+	const onSubmit = (e) => {
+		e.preventDefault();
+		handleSubmit(formData);
+	};
 
-  return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-      <motion.div
-        initial={{ height: 0, opacity: 0 }}
-        animate={{ height: "auto", opacity: 1 }}
-        exit={{ height: 0, opacity: 0 }}
-        transition={{ height: { duration: 0.5 }, opacity: { duration: 0.2 } }}
-        className="bg-base-100 rounded-lg p-6  shadow-lg w-full max-w-md sm:max-w-lg lg:max-w-xl overflow-hidden"
-      >
-        {/* <IoMdCloseCircle className='hover:cursor-pointer' /> */}
-        <button
-          type="button"
-          onClick={closeForm}
-          className="float-right hover:cursor-pointer hover:bg-primary rounded"
-        >
-          <AiOutlineCloseCircle size={22} />
-        </button>
+	// Get today's date in yyyy-mm-dd format
+	const today = new Date().toISOString().split('T')[0];
 
-        <h2 className="text-2xl mb-4">Add Your Subscription</h2>
-        <form onSubmit={onSubmit}>
-          <div className="mb-4">
-            <label>Category</label>
-            <select
-              name="category"
-              value={formData.category}
-              onChange={handleChange}
-              className="w-full px-3 py-2 border rounded"
-              required
-            >
-              <option value="">Select a category</option>
-              <option value="Entertainment">Entertainment</option>
-              <option value="Reading">Reading</option>
-              <option value="Music">Music</option>
-              <option value="e-Learning">e-Learning</option>
-              <option value="Sports">Sports</option>
-              <option value="Gaming">Gaming</option>
-              <option value="other">other</option>
-            </select>
-          </div>
-          <div className="mb-4">
-            <label className="block">Platform Name</label>
-            <input
-              type="text"
-              name="platformName"
-              value={formData.platformName}
-              onChange={handleChange}
-              className="w-full px-3 py-2 border rounded"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block">Plan Name</label>
-            <input
-              type="text"
-              name="planName"
-              value={formData.plan.planName}
-              onChange={handlePlanChange}
-              className="w-full px-3 py-2 border rounded"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block">Price</label>
-            <input
-              type="number"
-              name="price"
-              value={formData.plan.price}
-              onChange={handlePlanChange}
-              className="w-full px-3 py-2 border rounded"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label>Max Members</label>
-            <input
-              type="number"
-              name="maxMembers"
-              value={formData.plan.maxMembers}
-              onChange={handlePlanChange}
-              required
-              className="w-full px-3 py-2 border rounded"
-            />
-          </div>
-          <div className="mb-4">
-            <label>Expiration Date</label>
-            <input
-              type="date"
-              name="expirationDate"
-              value={formData.expirationDate}
-              required
-              placeholder="yyyy-mm-dd"
-              onChange={handleChange}
-              className="w-full px-3 py-2 border rounded"
-            />
-          </div>
-          <div className="mb-4">
-            <label>Display Publicly</label>
-            <input
-              name="public"
-              type="checkbox"
-              checked={formData.public}
-              onChange={handlePublic}
-              className="py-2 ml-4 border rounded"
-            />
-          </div>
-          <button
-            className="btn bg-primary text-white px-4 py-2 rounded-md shadow-sm font-semibold hover:bg-sky-700"
-            type="submit"
-          >
-            Submit
-          </button>
-        </form>
-      </motion.div>
-    </div>
-  );
+	return (
+		<div className='fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50'>
+			<motion.div
+				initial={{ height: 0, opacity: 0 }}
+				animate={{ height: 'auto', opacity: 1 }}
+				exit={{ height: 0, opacity: 0 }}
+				transition={{ height: { duration: 0.5 }, opacity: { duration: 0.2 } }}
+				className='bg-base-100 rounded-lg p-6  shadow-lg w-full max-w-md sm:max-w-lg lg:max-w-xl overflow-hidden'
+			>
+				{/* <IoMdCloseCircle className='hover:cursor-pointer' /> */}
+				<button
+					type='button'
+					onClick={closeForm}
+					className='float-right hover:cursor-pointer hover:bg-primary rounded'
+				>
+					<AiOutlineCloseCircle size={22} />
+				</button>
+
+				<h2 className='text-2xl mb-4'>Add Your Subscription</h2>
+				<form onSubmit={onSubmit}>
+					<div className='mb-4'>
+						<label>Category</label>
+						<select
+							name='category'
+							value={formData.category}
+							onChange={handleChange}
+							className='w-full px-3 py-2 border rounded'
+							required
+						>
+							<option value=''>Select a category</option>
+							<option value='Entertainment'>Entertainment</option>
+							<option value='Reading'>Reading</option>
+							<option value='Music'>Music</option>
+							<option value='e-Learning'>e-Learning</option>
+							<option value='Sports'>Sports</option>
+							<option value='Gaming'>Gaming</option>
+							<option value='other'>other</option>
+						</select>
+					</div>
+					<div className='mb-4'>
+						<label className='block'>Platform Name</label>
+						<input
+							type='text'
+							name='platformName'
+							value={formData.platformName}
+							onChange={handleChange}
+							className='w-full px-3 py-2 border rounded'
+							required
+						/>
+					</div>
+					<div className='mb-4'>
+						<label className='block'>Plan Name</label>
+						<input
+							type='text'
+							name='planName'
+							value={formData.plan.planName}
+							onChange={handlePlanChange}
+							className='w-full px-3 py-2 border rounded'
+							required
+						/>
+					</div>
+					<div className='mb-4'>
+						<label className='block'>Price</label>
+						<input
+							type='number'
+							name='price'
+							value={formData.plan.price}
+							onChange={handlePlanChange}
+							className='w-full px-3 py-2 border rounded'
+							required
+							min='0.01'
+						/>
+					</div>
+					<div className='mb-4'>
+						<label>Max Members</label>
+						<input
+							type='number'
+							name='maxMembers'
+							value={formData.plan.maxMembers}
+							onChange={handlePlanChange}
+							required
+							className='w-full px-3 py-2 border rounded'
+							min='1'
+						/>
+					</div>
+					<div className='mb-4'>
+						<label>Expiration Date</label>
+						<input
+							type='date'
+							name='expirationDate'
+							value={formData.expirationDate}
+							required
+							placeholder='yyyy-mm-dd'
+							onChange={handleChange}
+							className='w-full px-3 py-2 border rounded'
+							min={today}
+						/>
+					</div>
+					<div className='mb-4'>
+						<label>Display Publicly</label>
+						<input
+							name='public'
+							type='checkbox'
+							checked={formData.public}
+							onChange={handlePublic}
+							className='py-2 ml-4 border rounded'
+						/>
+					</div>
+					<button
+						className='btn bg-primary text-white px-4 py-2 rounded-md shadow-sm font-semibold hover:bg-sky-700'
+						type='submit'
+					>
+						Submit
+					</button>
+				</form>
+			</motion.div>
+		</div>
+	);
 };
 
 export default AddSubsForm;
--- src/utils/icons.jsx
+++ src/utils/icons.jsx
@@ -1,103 +1,103 @@
-import { MdLocalMovies, MdOutlineSportsSoccer, MdApps } from "react-icons/md";
+import { MdLocalMovies, MdOutlineSportsSoccer, MdApps } from 'react-icons/md';
 import {
-  FaMusic,
-  FaSpotify,
-  FaPlaystation,
-  FaXbox,
-  FaGraduationCap,
-  FaBuilding,
-} from "react-icons/fa";
-import { IoLogoGameControllerA, IoMdArrowDropdown } from "react-icons/io";
-import { HiMiniSquaresPlus } from "react-icons/hi2";
-import { RiNetflixFill } from "react-icons/ri";
-import { TbBrandHbo, TbBrandDisney } from "react-icons/tb";
+	FaMusic,
+	FaSpotify,
+	FaPlaystation,
+	FaXbox,
+	FaGraduationCap,
+	FaBuilding,
+} from 'react-icons/fa';
+import { IoLogoGameControllerA, IoMdArrowDropdown } from 'react-icons/io';
+import { HiMiniSquaresPlus } from 'react-icons/hi2';
+import { RiNetflixFill } from 'react-icons/ri';
+import { TbBrandHbo, TbBrandDisney } from 'react-icons/tb';
 import {
-  SiAppletv,
-  SiApplemusic,
-  SiYoutubemusic,
-  SiPrime,
-  SiParamountplus,
-  SiDazn,
-  SiAdobe,
-  SiCrunchyroll,
-} from "react-icons/si";
+	SiAppletv,
+	SiApplemusic,
+	SiYoutubemusic,
+	SiPrime,
+	SiParamountplus,
+	SiDazn,
+	SiAdobe,
+	SiCrunchyroll,
+} from 'react-icons/si';
 
 export const categoryIcons = {
-  All: <MdApps className="absolute top-2 -right-2 text-5xl" />,
-  "Select your category": (
-    <IoMdArrowDropdown className="absolute top-1/2 bottom-1/2 -translate-y-1/2 right-4 text-3xl" />
-  ),
-  Entertainment: <MdLocalMovies className="absolute top-2 -right-2 text-5xl" />,
-  Music: <FaMusic className="absolute top-2 right-0 text-4xl" />,
-  Gaming: (
-    <IoLogoGameControllerA className="absolute top-0 -right-2 text-6xl" />
-  ),
-  "e-Learning": (
-    <FaGraduationCap className="absolute top-2 -right-2 text-4xl" />
-  ),
-  Office: <FaBuilding className="absolute top-2 -right-2 text-4xl" />,
-  Sports: (
-    <MdOutlineSportsSoccer className="absolute top-2 -right-2 text-5xl" />
-  ),
-  Other: <HiMiniSquaresPlus className="absolute top-2 right-0 text-4xl" />,
+	All: <MdApps className='absolute top-2 -right-2 text-5xl' />,
+	'Select your category': (
+		<IoMdArrowDropdown className='absolute top-1/2 bottom-1/2 -translate-y-1/2 right-4 text-3xl' />
+	),
+	Entertainment: <MdLocalMovies className='absolute top-2 -right-2 text-5xl' />,
+	Music: <FaMusic className='absolute top-2 right-0 text-4xl' />,
+	Gaming: (
+		<IoLogoGameControllerA className='absolute top-0 -right-2 text-6xl' />
+	),
+	'e-Learning': (
+		<FaGraduationCap className='absolute top-2 -right-2 text-4xl' />
+	),
+	Office: <FaBuilding className='absolute top-2 -right-2 text-4xl' />,
+	Sports: (
+		<MdOutlineSportsSoccer className='absolute top-2 -right-2 text-5xl' />
+	),
+	Others: <HiMiniSquaresPlus className='absolute top-2 right-0 text-4xl' />,
 };
 
 export const platformName = {
-  Netflix: (
-    <RiNetflixFill className="absolute inset-y-0 right-2 translate-y-1/3  text-2xl text-red-700" />
-  ),
-  Amazon: (
-    <SiPrime className="absolute inset-y-0 right-2 translate-y-1/3 text-2xl text-[#219BFF] " />
-  ),
-  HBO: (
-    <TbBrandHbo className="absolute inset-y-0 right-2 translate-y-1/3 text-2xl " />
-  ),
-  Paramount: (
-    <SiParamountplus className="absolute inset-y-0 right-2 translate-y-1/3 text-2xl  " />
-  ),
-  Disney: (
-    <TbBrandDisney className="absolute inset-y-0 right-2 translate-y-1/3 text-2xl  " />
-  ),
-  "Apple TV": (
-    <SiAppletv className="absolute inset-y-0 right-2 translate-y-1/3 text-2xl  " />
-  ),
-  Spotify: (
-    <FaSpotify className="absolute inset-y-0 right-2 translate-y-1/3 text-2xl text-green-500 bg-black rounded-full" />
-  ),
-  "Apple Music": (
-    <SiApplemusic className="absolute inset-y-0 right-2 translate-y-1/3 text-2xl text-[#FB5A72] bg-white rounded-lg" />
-  ),
-  Youtube: (
-    <SiYoutubemusic className="absolute inset-y-0 right-2 translate-y-1/3 text-2xl text-[#FF0808] bg-white rounded-full " />
-  ),
-  Playstation: (
-    <FaPlaystation className="absolute inset-y-0 right-2 translate-y-1/3 text-2xl " />
-  ),
-  PS: (
-    <FaPlaystation className="absolute inset-y-0 right-2 translate-y-1/3 text-2xl " />
-  ),
-  Xbox: (
-    <FaXbox className="absolute inset-y-0 right-2 translate-y-1/3 text-2xl" />
-  ),
-  Adobe: <SiAdobe className="text-red-500" />,
-  Crunchyroll: <SiCrunchyroll className="text-orange-400" />,
+	Netflix: (
+		<RiNetflixFill className='absolute inset-y-0 right-2 translate-y-1/3  text-2xl text-red-700' />
+	),
+	Amazon: (
+		<SiPrime className='absolute inset-y-0 right-2 translate-y-1/3 text-2xl text-[#219BFF] ' />
+	),
+	HBO: (
+		<TbBrandHbo className='absolute inset-y-0 right-2 translate-y-1/3 text-2xl ' />
+	),
+	Paramount: (
+		<SiParamountplus className='absolute inset-y-0 right-2 translate-y-1/3 text-2xl  ' />
+	),
+	Disney: (
+		<TbBrandDisney className='absolute inset-y-0 right-2 translate-y-1/3 text-2xl  ' />
+	),
+	'Apple TV': (
+		<SiAppletv className='absolute inset-y-0 right-2 translate-y-1/3 text-2xl  ' />
+	),
+	Spotify: (
+		<FaSpotify className='absolute inset-y-0 right-2 translate-y-1/3 text-2xl text-green-500 bg-black rounded-full' />
+	),
+	'Apple Music': (
+		<SiApplemusic className='absolute inset-y-0 right-2 translate-y-1/3 text-2xl text-[#FB5A72] bg-white rounded-lg' />
+	),
+	Youtube: (
+		<SiYoutubemusic className='absolute inset-y-0 right-2 translate-y-1/3 text-2xl text-[#FF0808] bg-white rounded-full ' />
+	),
+	Playstation: (
+		<FaPlaystation className='absolute inset-y-0 right-2 translate-y-1/3 text-2xl ' />
+	),
+	PS: (
+		<FaPlaystation className='absolute inset-y-0 right-2 translate-y-1/3 text-2xl ' />
+	),
+	Xbox: (
+		<FaXbox className='absolute inset-y-0 right-2 translate-y-1/3 text-2xl' />
+	),
+	Adobe: <SiAdobe className='text-red-500' />,
+	Crunchyroll: <SiCrunchyroll className='text-orange-400' />,
 };
 
 // Function to get the platform icon based on the platform name
 export const getPlatformIcon = (platform) => {
-  // Convert the platform name to lowercase for case-insensitive comparison
-  const platformLowercase = platform?.toLowerCase();
+	// Convert the platform name to lowercase for case-insensitive comparison
+	const platformLowercase = platform?.toLowerCase();
 
-  // Iterate over each key in the platformName object
-  for (const key in platformName) {
-    // Convert the key to lowercase for case-insensitive comparison
-    const keyLowercase = key.toLowerCase();
-    // Check if the platform name includes the key as a substring
-    if (platformLowercase.includes(keyLowercase)) {
-      // Return the corresponding icon if there's a match
-      return platformName[key];
-    }
-  }
-  // Return null if there's no match (or for other platforms)
-  return null;
+	// Iterate over each key in the platformName object
+	for (const key in platformName) {
+		// Convert the key to lowercase for case-insensitive comparison
+		const keyLowercase = key.toLowerCase();
+		// Check if the platform name includes the key as a substring
+		if (platformLowercase.includes(keyLowercase)) {
+			// Return the corresponding icon if there's a match
+			return platformName[key];
+		}
+	}
+	// Return null if there's no match (or for other platforms)
+	return null;
 };
